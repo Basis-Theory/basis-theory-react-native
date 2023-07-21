a9611c050cf10de383547ed18a2baad6d83b9696
Brian Gonzalez <brian.gonzalez@basistheory.com>
2023-07-21 14:48:50 -0600

    chore: adds workflows and setup for CICD

---
+++ scripts/verify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+current_directory="$PWD"
+
+cd $(dirname $0)/..
+
+yarn lint
+
+#echo "Running tests..."
+
+#yarn test
+
+result=$?
+
+cd "$current_directory"
+
+exit $result
