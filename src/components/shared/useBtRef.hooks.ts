52db6794238e3a00aff803bb372648ee8ea96493
Kevin Peraza <kevin.peraza@live.com>
2023-09-21 16:01:39 -0600

    feat: allow to set expiration date from Card token (#23)
    * feat: bump bt deps
    
    * feat: allow to set expiration date from Card token

--- src/components/shared/useBtRef.hooks.ts
+++ src/components/shared/useBtRef.hooks.ts
@@ -5,7 +5,12 @@ import {
   SetStateAction,
   useEffect,
 } from 'react';
-import { BTRef, InputBTRef } from '../../BaseElementTypes';
+import {
+  BTRef,
+  InputBtDateRef,
+  InputBTRef,
+  ValueSetter,
+} from '../../BaseElementTypes';
 import { _elementValues } from '../../ElementValues';
 import { TextInput } from 'react-native';
 
@@ -16,6 +21,37 @@ interface UseBtRefProps {
   setTextInputValue: Dispatch<SetStateAction<string>>;
 }
 
+type CreateBtRefArgs = {
+  valueSetter: ValueSetter;
+} & Omit<UseBtRefProps, 'setTextInputValue' | 'btRef'>;
+
+const createBtRef = ({ id, textInputRef, valueSetter }: CreateBtRefArgs) => ({
+  id,
+  format: (plaintextValue: string) => plaintextValue,
+  clear: () => textInputRef?.current?.clear(),
+  focus: () => textInputRef?.current?.focus(),
+  blur: () => textInputRef?.current?.blur(),
+  setValue: valueSetter,
+});
+
+const formatValue = (ref: InputBTRef) => {
+  const value = _elementValues[ref.id];
+  return ref.format(typeof value === 'string' ? value : JSON.stringify(value));
+};
+
+const updateRef = (btRef: ForwardedRef<BTRef>, newBtRef: BTRef) => {
+  if (typeof btRef === 'function') {
+    btRef(newBtRef);
+  } else if (btRef && typeof btRef === 'object') {
+    btRef.current = newBtRef;
+  }
+};
+
+const isInputBtDateRef = (
+  ref: InputBTRef | InputBtDateRef
+): ref is InputBtDateRef =>
+  Boolean((ref as InputBtDateRef).month && (ref as InputBtDateRef).year);
+
 export const useBtRef = ({
   btRef,
   textInputRef,
@@ -23,34 +59,25 @@ export const useBtRef = ({
   setTextInputValue,
 }: UseBtRefProps) => {
   useEffect(() => {
-    const newBtRef = {
-      id,
-      format: (plaintextValue: string) => plaintextValue,
-      clear: () => {
-        textInputRef?.current?.clear();
-      },
-      focus: () => {
-        textInputRef?.current?.focus();
-      },
-      blur: () => {
-        textInputRef?.current?.blur();
-      },
-      setValue: (inputBtRef: InputBTRef) => {
-        const elementValue = _elementValues[inputBtRef.id];
-        const newTextInputValue = inputBtRef.format(
-          typeof elementValue === 'string'
-            ? elementValue
-            : JSON.stringify(elementValue)
+    const valueSetter: ValueSetter = (ref) => {
+      if (!ref) {
+        setTextInputValue('');
+      } else if (isInputBtDateRef(ref)) {
+        const formattedYear = formatValue(ref.year);
+        const formattedMonth = formatValue(ref.month);
+
+        setTextInputValue(
+          `${formattedMonth.padStart(2, '0')}/${formattedYear.slice(-2)}`
         );
-        setTextInputValue(newTextInputValue);
-      },
+      } else {
+        const val = formatValue(ref);
+
+        setTextInputValue(val);
+      }
     };
 
-    if (typeof btRef === 'function') {
-      btRef(newBtRef);
-    } else if (btRef && typeof btRef === 'object') {
-      // eslint-disable-next-line no-param-reassign
-      btRef.current = newBtRef;
-    }
+    const newBtRef = createBtRef({ id, textInputRef, valueSetter });
+
+    updateRef(btRef!!, newBtRef);
   }, [btRef, textInputRef, id]);
 };
