5c896aa029ae120532c932fed2e4f388f6e42bd2
kevin <kevin@basistheory.com>
2023-10-12 10:00:01 -0600

    feat: exports TextElement

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import 'react-native-url-polyfill/auto';
 import { CardNumberElement } from './components/CardNumberElement';
 import { CardVerificationCodeElement } from './components/CardVerificationCodeElement';
 import { CardExpirationDateElement } from './components/CardExpirationDateElement';
+import { TextElement } from './components/TextElement';
 import { BasisTheoryElements } from './BasisTheoryElements';
 import type { BTRef, InputBTRef } from './BaseElementTypes';
 
 export type { BTRef, InputBTRef };
 export {
+  BasisTheoryElements,
   CardNumberElement,
   CardVerificationCodeElement,
   CardExpirationDateElement,
-  BasisTheoryElements,
+  TextElement,
 };
