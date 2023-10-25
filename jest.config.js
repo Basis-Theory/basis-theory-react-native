d140b78990fc164fc8c1292a59fa1101cc5b05f9
kevin <kevin@basistheory.com>
2023-10-24 21:03:55 -0600

    chore: move jest config to its own file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  displayName: 'Jest',
+  transformIgnorePatterns: [],
+  transform: { '\\.(js|jsx|ts|tsx)$': '@sucrase/jest-plugin' },
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      lines: 80,
+      functions: 80,
+    },
+  },
+};
