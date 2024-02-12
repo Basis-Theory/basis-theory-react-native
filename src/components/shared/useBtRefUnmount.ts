5c8557e6c5b59d61fe6ba4da640b29a077007802
Kevin Peraza <kevin@basistheory.com>
2024-02-12 16:01:05 -0600

    feat: upgrade deps (#37)
    * chore: upgrade ts
    
    * chore: upgrade sucrase
    
    * chore: upgrade babel packages
    
    * chore: upgrade eslint
    
    * chore: remove get off my lawn
    
    * chore: upgrade types
    
    * chore: upgrade tsconfig rn
    
    * chore: upgrade testing library
    
    * chore: update lockfile
    
    * chore: upgrade semantic release
    
    * chore: bump btjs
    
    * chore: bump @react-navigation
    
    * chore: upgrade card-validator
    
    * chore: upgrade react-native-* packages
    
    * feat: upgrade rn
    
    * fix: add missing dev dep
    
    * ci: bump node in gh workflows
    
    * fix: temporarily disable snyk alert until fix is merged
    
    * refactor: remove unneded config

--- src/components/shared/useBtRefUnmount.ts
+++ src/components/shared/useBtRefUnmount.ts
@@ -1,17 +1,18 @@
-import type { ForwardedRef} from 'react';
+import type { ForwardedRef } from 'react';
 import { useEffect } from 'react';
 import type { BTRef } from '../../BaseElementTypes';
 
 type UseBtRefUnmountProps = {
   btRef?: ForwardedRef<BTRef>;
-}
+};
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useBtRefUnmount = ({ btRef }: UseBtRefUnmountProps) => {
   useEffect(
     () => () => {
       // this suggests to have btRef as the dep, but we only want to set btRef to
       // null once on unmount
-      // eslint-disable-next-line react-hooks/exhaustive-deps, no-param-reassign, unicorn/no-null
+      // eslint-disable-next-line no-param-reassign,
       btRef = null;
     },
     []
